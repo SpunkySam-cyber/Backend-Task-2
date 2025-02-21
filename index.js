5e1b94fb5f6e19b185f5c03b035d3b6f030bee82
Sumaya Hashim <sumaiyahafiz7@gmail.com>
2025-02-21 19:25:00 +0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('<h1> Hello from deployed version</h1>')
+})
+
+app.listen(PORT, () =>{
+    console.log(`Server running on port ${PORT}`)
+})
