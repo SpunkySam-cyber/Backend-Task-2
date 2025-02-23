479e4a2a6f16e07cc07a5531357056c378d71640
Sumaya Hashim <64749377+SpunkySam-cyber@users.noreply.github.com>
2025-02-23 22:45:02 +0500

    Update server.js

--- server.js
+++ server.js
@@ -46,5 +46,3 @@ app.get('*', (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-import favicon from 'serve-favicon';
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
